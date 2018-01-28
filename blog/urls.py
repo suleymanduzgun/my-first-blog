78accc41813436a0c41a6de4f60b9b5d55d18130
suleymanduzgun <suleymanduzgun@gmail.com>
2018-01-28 19:08:56 +0300

    add

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 #from django.shortcuts import render
 
-
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
